4adbe2ebda7beca0cfa0ac1b0ab2e1a3da446b15
vcosmin2701 <v.cosmin2717@gmail.com>
2025-01-01 16:23:33 +0200

    File to be tested

---
+++ test.swift
@@ -0,0 +1,26 @@
+struct userAccount{
+    var userName:String
+var userAge:Int
+            var isActive : Bool
+    
+    func printUserDetails(){print("\(userName) is \(userAge) years old")
+        if(isActive==true){
+            print("Account is active")
+        }
+        else {
+            print("Account is inactive")
+        }
+    }
+    
+    
+func calculateDiscount()->Double{
+        var discount:Double = 0.0
+    if userAge>60{discount=0.2}
+        else if(userAge>30){
+            discount = 0.1
+    }
+    return discount
+}}
+
+let newUser=userAccount(userName: "john",userAge:25,isActive:true)
+newUser.printUserDetails()
