e89915c0f5bdf3fc5214ad7b3f3821c1e65c3626
vcosmin2701 <v.cosmin2717@gmail.com>
2025-01-19 22:46:46 +0200

    Raw values for enums

--- OOP/enumarations.swift
+++ OOP/enumarations.swift
@@ -40,4 +40,37 @@ if color == .Blue {
     print("\(color) edition")
 }
 
+// RAW VALUES
 
+// Each raw value must be unique
+
+enum CarColor: Int {
+    case Black = 2
+    case Blue = 4
+    case OffWhite = 5
+    case Silver = 6
+}
+
+var colorCount = CarColor.Black.rawValue
+print("Raw value of \(CarColor.Black): \(colorCount)")
+
+enum SubjectMarks: Int {
+    case maths = 40, english, hindi, physics
+}
+
+let marks = SubjectMarks.english.rawValue
+print("Mark for \(SubjectMarks.english): \(marks)")
+
+// Initializing from a raw value
+
+enum InitEnum: String {
+    case value1 = "Enum 1"
+    case value2 = "Enum 2"
+    case value3 = "Enum 3"
+}
+
+if let result = InitEnum(rawValue: "Enum 1") {
+    print("Case '\(result)' is found for the given raw value")
+} else {
+    print("No match found")
+}
